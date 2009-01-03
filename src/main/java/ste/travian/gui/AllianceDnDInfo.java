e3f2f38e7071eb67fd63f505f42345572ec6568d
Stefano Fornari <stefano@simste.it>
2009-01-03 09:12:23 +0100

    reworking of the alliance group dialog

---
+++ src/main/java/ste/travian/gui/AllianceDnDInfo.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2008
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Derived from Travian world" logo. If the display of the logo is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Derived from Travian world".
+ */
+
+package ste.travian.gui;
+
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Vector;
+
+public class AllianceDnDInfo implements Transferable, Serializable {
+
+  final public static DataFlavor INFO_FLAVOR =
+    new DataFlavor(AllianceDnDInfo.class, "Alliance");
+
+  static DataFlavor flavors[] = { INFO_FLAVOR };
+
+  private String name = null;
+  private AllianceDnDInfo parent = null;
+  private Vector children = null;
+
+
+  public AllianceDnDInfo(String name) {
+    children = new Vector();
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void add(AllianceDnDInfo info) {
+    info.setParent(this);
+    children.add(info);
+  }
+
+  public void remove(AllianceDnDInfo info) {
+    info.setParent(null);
+    children.remove(info);
+  }
+
+  public AllianceDnDInfo getParent() {
+    return parent;
+  }
+
+  public void setParent(AllianceDnDInfo parent) {
+    parent = parent;
+  }
+
+  public Vector getChildren() {
+    return children;
+  }
+
+  public Object clone() {
+    return new AllianceDnDInfo(name);
+  }
+
+  public String toString() {
+    return name;
+  }
+
+  // -------------------------------------------------------------- Transferable
+
+  public boolean isDataFlavorSupported(DataFlavor df) {
+    return df.equals(INFO_FLAVOR);
+  }
+
+  /** implements Transferable interface */
+  public Object getTransferData(DataFlavor df)
+      throws UnsupportedFlavorException, IOException {
+    if (df.equals(INFO_FLAVOR)) {
+      return this;
+    }
+    else throw new UnsupportedFlavorException(df);
+  }
+
+  /** implements Transferable interface */
+  public DataFlavor[] getTransferDataFlavors() {
+    return flavors;
+  }
+
+  // -------------------------------------------------------------- Serializable
+
+   private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+     out.defaultWriteObject();
+   }
+
+   private void readObject(java.io.ObjectInputStream in)
+     throws IOException, ClassNotFoundException {
+     in.defaultReadObject();
+   }
+}
---
+++ src/main/java/ste/travian/gui/AllianceGroupsPanel.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2008
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301 USA.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License
+ * version 3, these Appropriate Legal Notices must retain the display of the
+ * "Derived from Travian world" logo. If the display of the logo is not
+ * reasonably feasible for technical reasons, the Appropriate Legal Notices must
+ * display the words "Derived from Travian world".
+ */
+package ste.travian.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Toolkit;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+public class AllianceGroupsPanel extends JPanel {
+
+    private AllianceGroupsTree tree;
+    private JList restOfTheWorldList;
+    private Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+    public AllianceGroupsPanel(Frame frame) {
+        super(new BorderLayout());
+
+        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+
+        tree = new AllianceGroupsTree(frame);
+        add(new JScrollPane(tree), BorderLayout.LINE_START);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JButton("+"));
+        panel.add(new JButton("-"));
+        panel.setPreferredSize(new Dimension(40, 100));
+        add(panel, BorderLayout.CENTER);
+
+        restOfTheWorldList = new AllianceList();
+        JScrollPane pane = new JScrollPane(restOfTheWorldList);
+        pane.setPreferredSize(new Dimension(200, 300));
+        add(pane, BorderLayout.LINE_END);
+    }
+
+    /** Remove all nodes except the root node. */
+    public void clear() {
+        TreeModel model = tree.getModel();
+        ((DefaultMutableTreeNode)model.getRoot()).removeAllChildren();
+    }
+
+    /** Remove the currently selected node. */
+    public void removeCurrentNode() {
+        TreePath currentSelection = tree.getSelectionPath();
+        if (currentSelection != null) {
+            DefaultMutableTreeNode currentNode = (DefaultMutableTreeNode) (currentSelection.getLastPathComponent());
+            MutableTreeNode parent = (MutableTreeNode) (currentNode.getParent());
+            if (parent != null) {
+                ((DefaultTreeModel)tree.getModel()).removeNodeFromParent(currentNode);
+                return;
+            }
+        }
+
+        // Either there was no selection, or the root was selected.
+        toolkit.beep();
+    }
+
+    public AllianceGroupsTree getTree() {
+        return tree;
+    }
+
+    public JList getRestOfTheWorldList() {
+        return restOfTheWorldList;
+    }
+}
