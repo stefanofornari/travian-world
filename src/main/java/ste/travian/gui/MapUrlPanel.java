7c931fef76916108ab9ff05c61d83d85c8a7d52d
Stefano Fornari <stefano@simste.it>
2009-01-25 18:36:35 +0100

    fixed copyright

--- src/main/java/ste/travian/gui/MapUrlPanel.java
+++ src/main/java/ste/travian/gui/MapUrlPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008
+ * Copyright (C) 2009
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by
